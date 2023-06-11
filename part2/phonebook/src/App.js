3288e8d2823abfcf5dc62fa01f7a424d229521e9
clewandowski32 <lewandoc@umich.edu>
2023-06-11 15:59:52 -0400

    up to 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, handleFilterChange}) => {
   return(
@@ -48,15 +49,26 @@ const Persons = ({personsToShow}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  console.log('render', persons.length, 'notes')
+
+  useEffect(hook, [])
+
+
 
   const handleFilterChange = (event) => {
     console.log(event.target.value)
@@ -103,7 +115,6 @@ const App = () => {
                   handleNameChange={handleNameChange} newNumber={newNumber}
                   handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
-      {/* {personsToShow.map(person => <div key={person.id}>{person.name} {person.number}</div>)} */}
       <Persons personsToShow={personsToShow}/>
     </div>
   )
